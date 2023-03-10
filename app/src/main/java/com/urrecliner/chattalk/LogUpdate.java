ecc9dd18ba4e4a795c0bbf4eae239aaf4b0c1f47
Woncherl Ha (하원철) <riopapa@gmail.com>
2023-03-10 19:41:13 +0900

    Add LogStock Tab

--- app/src/main/java/com/urrecliner/chattalk/LogUpdate.java
+++ app/src/main/java/com/urrecliner/chattalk/LogUpdate.java
@@ -23,6 +23,73 @@ import java.util.Locale;
 
 public class LogUpdate {
 
+    public LogUpdate(Context context) {
+        mContext = context;
+        if (sharePref == null) {
+            sharePref = mContext.getSharedPreferences("sayText", MODE_PRIVATE);
+            sharedEditor = sharePref.edit();
+        }
+        logQue = sharePref.getString("logQue", "");
+        logStock = sharePref.getString("logStock", "");
+        logSave = sharePref.getString("logSave", "");
+    }
+
+    final SimpleDateFormat MMDDHHMM = new SimpleDateFormat("MM-dd HH:mm ", Locale.KOREA);
+    void addQue(String header, String text) {
+        readyTodayFolderIfNewDay();
+        logQue += "\n" + MMDDHHMM.format(new Date())
+                + header + "\n" + text+"\n";
+        int len = logQue.length();
+        if (len > 15000) {   // max log que size
+            logQue = logQue.substring(5000);    // remove old 3000 bytes
+            logQue = logQue.substring(logQue.indexOf("\n")+1);
+            if (!StringUtils.isNumeric(""+logQue.charAt(0))) {  // start with MMDD ...
+                logQue = logQue.substring(logQue.indexOf("\n")+1);
+            }
+            String front = logQue.substring(0, logQue.length()*2/3);
+            front = front.substring(0, front.lastIndexOf("\n"));
+            int pos = front.lastIndexOf("\n", front.length()-2);
+            if (!StringUtils.isNumeric(""+front.charAt(pos))) {  // start with MMDD ...
+                pos = front.lastIndexOf("\n", pos-2);
+            }
+            front = logQue.substring(0, pos).replace("\n\n","\n");
+            front = front.replace("\n\n","\n");
+            String remain = logQue.substring(pos);
+            logQue = front+"\n\n/** " + MMDDHHMM.format(new Date()) + " **/" +
+                    "\n---- squeezed to "+ (front.length()+remain.length()) + " -------" +
+                    "\n"+remain +
+                    "\n---- squeezed " + MMDDHHMM.format(new Date()) + "\n";
+        }
+        sharedEditor.putString("logQue", logQue);
+        sharedEditor.apply();
+    }
+    void addStock(String header, String text) {
+        readyTodayFolderIfNewDay();
+        logStock += "\n" + MMDDHHMM.format(new Date())
+                + header + "\n" + text+"\n";
+        int len = logStock.length();
+        if (len > 5000) {   // max log que size
+            logStock = logStock.substring(2000);    // remove old 3000 bytes
+            logStock = logStock.substring(logStock.indexOf("\n")+1);
+            if (!StringUtils.isNumeric(""+logStock.charAt(0))) {  // start with MMDD ...
+                logStock = logStock.substring(logStock.indexOf("\n")+1);
+            }
+            String front = logStock.substring(0, logStock.length()*2/3);
+            front = front.substring(0, front.lastIndexOf("\n"));
+            int pos = front.lastIndexOf("\n", front.length()-2);
+            if (!StringUtils.isNumeric(""+front.charAt(pos))) {  // start with MMDD ...
+                pos = front.lastIndexOf("\n", pos-2);
+            }
+            front = logStock.substring(0, pos).replace("\n\n","\n");
+            front = front.replace("\n\n","\n");
+            String remain = logStock.substring(pos);
+            logStock = front+"\n\n/** " + MMDDHHMM.format(new Date()) + " **/" +
+                    "\n---- squeezed to "+ (front.length()+remain.length()) + " -------" +
+                    "\n"+remain +
+                    "\n---- squeezed " + MMDDHHMM.format(new Date()) + "\n";
+        }
+        sharedEditor.putString("logStock", logStock);
+        sharedEditor.apply();
     }
 
     public void readyTodayFolderIfNewDay() {
